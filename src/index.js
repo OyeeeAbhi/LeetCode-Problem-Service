6731f99373613fa0496bb3d72a4c083920c72d8d
OyeeeAbhi <abhishekmech1002@gmail.com>
2024-10-11 20:05:10 +0530

    adedd routing version for all api's

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+
 const {PORT} = require('./config/server.js');
+const apiRouter = require('./routes/index.js');
+
 const app = express();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.text());
+
+app.use('/api' , apiRouter);
 
 app.get('/ping' , (req , res) =>{
     return res.json({message : 'Problem service is alive'});
