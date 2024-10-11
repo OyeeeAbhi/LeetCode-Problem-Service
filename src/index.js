47253e46228f8cadf6779a8852a888227e040257
OyeeeAbhi <abhishekmech1002@gmail.com>
2024-10-11 19:31:23 +0530

    addedd basic express server setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {PORT} = require('./config/server.js');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.text());
+
+app.get('/ping' , (req , res) =>{
+    return res.json({message : 'Problem service is alive'});
+})
+
+app.listen(PORT , ()=>{
+    console.log('listening on port : ' , PORT);
+})
