194b823826602a445a46fec7a01deba41549c3fc
OyeeeAbhi <abhishekmech1002@gmail.com>
2024-10-11 20:20:52 +0530

    adedd not implemented error handling for initial version of code

--- src/controller/problem.controller.js
+++ src/controller/problem.controller.js
@@ -1,25 +1,28 @@
+const httpStatusCodes = require('http-status-codes');
+
+
 function pingProblemController(req , res){
     return res.json({message : 'ping problem controller is up'});
 }
 
 function addProblem(req , res){
-
+    return res.status(httpStatusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'})
 }
 
 function getProblem(req , res){
-
+    return res.status(httpStatusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'})
 }
 
 function getProblems(req , res){
-
+    return res.status(httpStatusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'})
 }
 
 function deleteProblem(req , res){
-
+    return res.status(httpStatusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'})
 }
 
 function updateProblem(req , res){
-
+    return res.status(httpStatusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'})
 }
 
 module.exports = {
